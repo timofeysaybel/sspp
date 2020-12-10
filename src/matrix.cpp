4ba0b06df5b0d620db18457c30cb15888ef4fd56
Timofey <saybeltimofey@gmail.com>
2020-12-10 21:31:59 +0300

    task4

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -196,7 +196,7 @@ vector<double> Matrix::parallelMultiply(const vector<double> &b,string fileC,str
 
         Time << (mode == 0 ? commSize : commSize + 100) << "\t" << time << endl;
         SpeedUp << (mode == 0 ? commSize : commSize + 100) << "\t" << cTime / time << endl;
-        Efficiency << (mode == 0 ? commSize : commSize + 100 << "\t" << (cTime / time) / commSize << endl;
+        Efficiency << (mode == 0 ? commSize : commSize + 100) << "\t" << (cTime / time) / commSize << endl;
     }
     else
     {
